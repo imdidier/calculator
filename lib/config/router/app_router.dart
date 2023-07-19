46da08233b3edaac6d07c151e7efb35272572cea
Didier David <71243788+imdidierjunco@users.noreply.github.com>
2023-07-18 21:52:10 -0500

    first commit

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,12 @@
+import 'package:calculator/ui/screens/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    )
+  ],
+);
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculator/config/router/app_router.dart';
+import 'package:calculator/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      theme: AppTheme().getTheme(),
+      routerConfig: appRouter,
+    );
+  }
+}
